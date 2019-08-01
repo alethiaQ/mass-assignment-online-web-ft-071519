a98ed050d0678a9773b7274910d4f839ad8e72e6
Alethia Quintero <alethiaq22@gmail.com>
2019-08-01 17:07:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,6 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attrib_collection)
-    attrib_collection.each {|attrib, desc| self.send (("#{attrib}="), desc)}
+    attrib_collection.each {|attrib, desc| self.send (("#{attrib}="),desc)}
   end
 end
